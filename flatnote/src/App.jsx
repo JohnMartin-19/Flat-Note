15d114253333d0ffc65bd906c2bfaf09d8ca52ba
John <johnnkonge2020@gmail.com>
2023-11-08 08:41:30 +0300

    fetched data

--- flatnote/src/App.jsx
+++ flatnote/src/App.jsx
@@ -1,7 +1,15 @@
 
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(()=> {
+    fetch("http://localhost:8000/Universities/")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+  },[])
   return (
     <div className="App">
       <h1>Hello John!</h1>
