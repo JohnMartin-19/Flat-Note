6c6fdb2e9e5e0173002b93060e04d3881fa881cc
John <johnnkonge2020@gmail.com>
2023-11-09 08:21:22 +0300

    modified header

--- flatnote/src/header.jsx
+++ flatnote/src/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header(){
     return (
-        <div style={{backgroundColor: 'rgb(9, 70, 9)',padding: '15px', borderRadius:'10px' }}>
+        <div style={{backgroundColor: 'rgb(9, 70, 9)',padding: '15px', borderRadius:'1px' }}>
             <h1 style={{textAlign: "center", color:"white" }}>Welcome To My FlatUniApp</h1>
                 
         </div>
