5598595c0fe386be7686c10c133c7bbbcfa18413
John <johnnkonge2020@gmail.com>
2023-11-10 14:47:37 +0300

    done

--- flatnote/src/LandingPg.jsx
+++ flatnote/src/LandingPg.jsx
@@ -5,7 +5,7 @@ function Landing(){
     return(
         <div class="container" >
             <h1>FlatUni</h1>
-            <p>Its an application created in react and has communication with the backend(josn server) to fetch data from the API, and display the data to the end user.<br/>
+            <p>Its an application created in react and has communication with the backend(json server) to fetch data from the API, and display the data to the end user.<br/>
             The user might as well be able to manipulate the data to his or her own liking</p>
         </div>
     )
